14465671c281be0b74d2c073122f09f6dbfbd3ce
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-04-15 23:30:25 -0400

    Failing unit tests for Arg- and Env- Acceptance, to add #silent?
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- components/ruby/spec/license_acceptance/arg_acceptance_spec.rb
+++ components/ruby/spec/license_acceptance/arg_acceptance_spec.rb
@@ -4,17 +4,51 @@ require "license_acceptance/arg_acceptance"
 RSpec.describe LicenseAcceptance::ArgAcceptance do
   let(:acc) { LicenseAcceptance::ArgAcceptance.new }
 
-  describe "#check" do
-    it "returns true if the args contain the required flag with spaces" do
-      expect(acc.check(["--chef-license", "accept"])).to eq(true)
+  describe "with an accept option" do
+    describe "#check" do
+      it "returns true if the args contain the required flag with spaces" do
+        expect(acc.check(["--chef-license", "accept"])).to eq(true)
+      end
+
+      it "returns true if the args contain the required flag with equal" do
+        expect(acc.check(["--chef-license=accept"])).to eq(true)
+      end
+
+      it "returns false if the args do not contain the required value" do
+        expect(acc.check(["--chef-license"])).to eq(false)
+      end
     end
 
-    it "returns true if the args contain the required flag with equal" do
-      expect(acc.check(["--chef-license=accept"])).to eq(true)
+    describe "#silent?" do
+      it "returns false if the args contain the required flag with spaces" do
+        expect(acc.silent?(["--chef-license", "accept"])).to eq(false)
+      end
+
+      it "returns false if the args contain the required flag with equal" do
+        expect(acc.silent?(["--chef-license=accept"])).to eq(false)
+      end
+    end
+  end
+
+  describe "with a silent option" do
+    describe "#check" do
+      it "returns true if the args contain the required flag with spaces" do
+        expect(acc.check(["--chef-license", "accept-silent"])).to eq(true)
+      end
+
+      it "returns true if the args contain the required flag with equal" do
+        expect(acc.check(["--chef-license=accept-silent"])).to eq(true)
+      end
     end
 
-    it "returns false if the args do not contain the required value" do
-      expect(acc.check(["--chef-license"])).to eq(false)
+    describe "#silent?" do
+      it "returns true if the args contain the required flag with spaces" do
+        expect(acc.silent?(["--chef-license", "accept-silent"])).to eq(true)
+      end
+
+      it "returns true if the args contain the required flag with equal" do
+        expect(acc.silent?(["--chef-license=accept-silent"])).to eq(true)
+      end
     end
   end
 
--- components/ruby/spec/license_acceptance/env_acceptance_spec.rb
+++ components/ruby/spec/license_acceptance/env_acceptance_spec.rb
@@ -4,21 +4,41 @@ require "license_acceptance/env_acceptance"
 RSpec.describe LicenseAcceptance::EnvAcceptance do
   let(:acc) { LicenseAcceptance::EnvAcceptance.new }
 
-  describe "#check" do
-    it "returns true if the env contains the correct key and value" do
-      env = {"CHEF_LICENSE" => "accept"}
-      expect(acc.check(env)).to eq(true)
+  describe "when passed an accept value" do
+    describe "#check" do
+      it "returns true if the env contains the correct key and value" do
+        env = {"CHEF_LICENSE" => "accept"}
+        expect(acc.check(env)).to eq(true)
+      end
+
+      it "returns false if the env has a key but nil value" do
+        env = {"CHEF_LICENSE" => nil}
+        expect(acc.check(env)).to eq(false)
+      end
+
+      it "returns false if the env has a key but incorrect value" do
+        env = {"CHEF_LICENSE" => "foo"}
+        expect(acc.check(env)).to eq(false)
+      end
     end
 
-    it "returns false if the env has a key but nil value" do
-      env = {"CHEF_LICENSE" => nil}
-      expect(acc.check(env)).to eq(false)
+    describe "#silent?" do
+      it "returns true if the env contains the correct key and value" do
+        env = {"CHEF_LICENSE" => "accept-silent"}
+        expect(acc.silent?(env)).to eq(true)
+      end
+
+      it "returns false if the env has a key but nil value" do
+        env = {"CHEF_LICENSE" => nil}
+        expect(acc.silent?(env)).to eq(false)
+      end
+
+      it "returns false if the env has a key but incorrect value" do
+        env = {"CHEF_LICENSE" => "foo"}
+        expect(acc.silent?(env)).to eq(false)
+      end
     end
 
-    it "returns false if the env has a key but incorrect value" do
-      env = {"CHEF_LICENSE" => "foo"}
-      expect(acc.check(env)).to eq(false)
-    end
   end
 
   describe "#check_no_persist" do
