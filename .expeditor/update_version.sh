4f6f77ef162373cab4b60eafb876fb97ba3e88c0
tyler-ball <tball@chef.io>
2019-10-22 14:50:34 -0600

    Switching back to cd since I don't think the docker image supports pushd/popd
    Signed-off-by: tyler-ball <tball@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -7,9 +7,9 @@
 set -evx
 
 sed -i -r "s/^(\s*)VERSION = \".+\"/\1VERSION = \"$(cat VERSION)\"/" components/ruby/lib/license_acceptance/version.rb
-pushd components/ruby
+cd components/ruby
 bundle update license-acceptance
-popd ../..
+cd ../..
 sed -i -r "s/^pkg_version=.+\$/\pkg_version=$(cat VERSION)/" components/golang/habitat/plan.sh
 
 # Once Expeditor finshes executing this script, it will commit the changes and push
