f47a29b27d6c3809eb5ab5c4ad8d26b12590409f
Pete Higgins <pete@peterhiggins.org>
2020-09-03 17:55:00 -0700

    Add example Mixlib::CLI app using this library for human testing.
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

---
+++ components/ruby/example/example_mixlib_cli_app.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift File.join(File.expand_path("..", __dir__), "lib/")
+
+require "mixlib/cli"
+require "license_acceptance/acceptor"
+require "license_acceptance/cli_flags/mixlib_cli"
+require "logger"
+
+class ExampleMixlibCLIApp
+  include Mixlib::CLI
+  include LicenseAcceptance::CLIFlags::MixlibCLI
+
+  option :help,
+    short: "-h",
+    long: "--help",
+    description: "Show this message",
+    on: :tail,
+    boolean: true,
+    show_options: true,
+    exit: 0
+
+  def run(argv = ARGV)
+    unless ARGV.include?("--help") || ARGV.include?("-h")
+      options = {}
+
+      if ENV["LOG_LEVEL"]
+        logger = Logger.new(STDOUT)
+        logger.level = ENV["LOG_LEVEL"]
+
+        options[:logger] = logger
+      end
+
+      if ENV["DEBUG_LICENSE"]
+        options[:provided] = ENV["DEBUG_LICENSE"]
+      end
+
+      LicenseAcceptance::Acceptor.check_and_persist!("infra-client", "1337", options)
+    end
+
+    parse_options(argv)
+  end
+end
+
+cli = ExampleMixlibCLIApp.new
+cli.run
+
+puts "BIG SUCCESS"
